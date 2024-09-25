cb23c3262ec8d0f57d9af95b07c5c0d4f1d26cf4
sphanse2021 <87150932+sphanse2021@users.noreply.github.com>
2024-09-25 12:22:47 -0400

    Update streamlit_app.py
    Moved API call statement and dataframe statement inside the For fruit_chosen loop.

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,29 +25,16 @@ ingredients_list = st.multiselect(
     , max_selections=5
 )
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
-
 if ingredients_list:
-    # st.write(ingredients_list);
-    # st.text(ingredients_list)
-
     ingredients_string = ''
-
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-
-    #st.write(ingredients_string)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """', '"""+ name_on_order+ """')"""
-    
-
-    # st.write(my_insert_stmt)
-    # st.stop()
     time_to_insert = st.button('Submit Order')
-    
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
